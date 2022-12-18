06258c8e0dbfb48af34a2d0c1403a3c3626b6c95
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-12-18 19:20:55 +0530

    navbar complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Navbar.module.css'
 
 const navbar = () => {
+  
   return (
       <div>
         {/* ******************************************Navbar***************************************** */}
-        <nav className={styles.mainnav}>
-        <ul>
-          <Link href="/"><li>Home</li></Link>
-          <Link href="/about"><li>About</li></Link>
-          <Link href="/blog"><li>blog</li></Link>
-          <Link href="/contact"><li>Contact</li></Link>
-        </ul>
-      </nav>
-  
+    <header class={styles.header}>
+      <a class={styles.logo} href=".page-top">WordlyThoughts</a>
+      <input class={styles.menubtn} type="checkbox" id="menu-btn" />
+      <label class={styles.menuicon} for="menu-btn"><span class={styles.navicon}></span></label>
+      <ul class={styles.menu}>
+        <Link href="/" ><li class={`${styles.link} ${styles.linktheme} ${styles.linkarrow}`}>HOME</li></Link>
+        <Link href="/about"><li class={`${styles.link} ${styles.linktheme} ${styles.linkarrow}`}>BLOG</li></Link>
+        <Link href="/blog" ><li class={`${styles.link} ${styles.linktheme} ${styles.linkarrow}`}>ABOUT US</li></Link>
+        <Link href="/contact" ><li class={`${styles.link} ${styles.linktheme} ${styles.linkarrow}`}>CONTACT</li></Link>
+      </ul>
+    </header>
       {/* ******************************************Navbar***************************************** */}
       </div>
   )
